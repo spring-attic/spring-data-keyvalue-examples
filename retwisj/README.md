b04ebfd60f5cad25fe8522ecc878990c6ed6792f
Costin Leau <cleau@vmware.com>
2011-07-24 20:54:49 +0300

    + update sample to Spring Redis 1.0 M4 and Spring 3.1 M2

--- retwisj/README.md
+++ retwisj/README.md
@@ -1,5 +1,5 @@
-Spring Data - Key Value - Redis Twitter Example
-===============================================
+Spring Data - Redis Twitter Example
+===================================
 
 An improved Java implementation of the [Redis Twitter Clone](http://redis.io/topics/twitter-clone) using Spring Data. Tutorial available [here](http://static.springsource.org/spring-data/data-keyvalue/examples/retwisj/current/)
 
--- retwisj/src/main/java/org/springframework/data/redis/samples/retwisj/web/RetwisController.java
+++ retwisj/src/main/java/org/springframework/data/redis/samples/retwisj/web/RetwisController.java
@@ -76,7 +76,9 @@ public class RetwisController {
 	}
 
 	@RequestMapping("/signIn")
-	public String signIn(String name, String pass, Model model, HttpServletResponse response) {
+	public String signIn(@RequestParam(required = false) String name, @RequestParam(required = false) String pass, Model model, HttpServletResponse response) {
+		System.out.println("name is " + name);
+		System.out.println("pass is " + name);
 		// add tracing cookie
 		if (retwis.auth(name, pass)) {
 			addAuthCookie(retwis.addAuth(name), name, response);
