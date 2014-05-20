7c1467da42d3fd313c25705d1636f140b038c56d
Thomas Darimont <tdarimont@gopivotal.com>
2014-05-20 10:46:18 +0200

    Rebuild example with Spring Boot
    Initial prototype.

---
+++ retwisj-reboot/src/main/java/org/example/retwisj/Range.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.example.retwisj;
+
+/**
+ * Basic object indicating a range of objects to retrieve. Default is 10 objects (starting at zero).
+ * 
+ * @author Costin Leau
+ */
+public class Range {
+
+	private static final int SIZE = 9;
+	public int begin = 0;
+	public int end = SIZE;
+
+	public Range() {
+	};
+
+	public Range(int begin, int end) {
+		this.begin = begin;
+		this.end = end;
+	}
+
+	public Range(int pageNumber) {
+		this.begin = 0;
+		this.end = pageNumber * SIZE;
+	}
+
+	public int getPages() {
+		return (int) Math.round(Math.ceil(end / SIZE));
+	}
+}
---
+++ retwisj-reboot/src/main/java/org/example/retwisj/web/WebUtils.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.example.retwisj.web;
+
+
+/**
+ * Utility converting the date (stored as long) into human-friendly message.
+ * 
+ * @author Costin Leau
+ */
+public class WebUtils {
+
+	public static String timeInWords(long time) {
+		long elapsed = System.currentTimeMillis() - time;
+
+		// seconds
+		elapsed /= 1000;
+
+		if (elapsed < 10) {
+			return "time.now";
+		}
+		if (elapsed < 60) {
+			return "time.minute.less";
+		}
+
+		// minutes
+		elapsed /= 60;
+
+		if (elapsed < 2) {
+			return "time.minute";
+		}
+
+		if (elapsed < 45) {
+			return "time.minutes#" + elapsed;
+		}
+
+		if (elapsed < 90) {
+			return "time.hour";
+		}
+
+		if (elapsed < 1440) {
+			return "time.hours#" + elapsed / 60;
+		}
+
+		if (elapsed < 2880) {
+			return "time.day";
+		}
+
+		if (elapsed < 43200) {
+			return "time.days#" + (elapsed / 1440);
+		}
+
+		if (elapsed < 86400) {
+			return "time.month";
+		}
+
+		if (elapsed < 525600) {
+			return "time.months#" + (elapsed / 43200);
+		}
+
+		if (elapsed < 1051199) {
+			return "time.year";
+		}
+
+		return "time.years#" + (elapsed / 525600);
+	}
+}
